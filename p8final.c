20cd0ccf9aa73d8b371c99341130ae6831b3dd95
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-03-09 04:20:20 +0000

    p8

--- p8final.c
+++ p8final.c
@@ -47,4 +47,26 @@ Fraction add_n_fractions(int n,Fraction f[n])
       sum=add_fractions(sum,f[i]);
     }
 }
+void ouput(Fraction f1,Fraction f2,Fraction sum)
+{
+  printf("the sum of fractions %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+}
+void output(Fraction f1,Fraction f2,Fraction sum)
+{
+  for(int i=0;i<n;i++)
+    {
+      printf("%d/%d+",f[i].num,f[i].den)
+    }
+  printf("%d/%d",f[i].num,f[i].den);
+  printf("is %d/%d\n",sum.num,sum.den);
+}
+int main()
+{
+  int n,sum;
+  n=input_n();
+  Fraction f[n],sum;
+  input_n_fraction(n,f);
+  sum=add_n_fractions(n,f);
+  output_n_fractions(n,f,sum);
+  return 0;
 }
